a9ff0bdf8b4fa6db58f66932193f5332a788cb8c
keepselvesreal <keep_selfs_real@naver.com>
2024-05-07 09:16:04 +0900

    first commit

---
+++ orderbot/asgi.py
@@ -0,0 +1,24 @@
+"""
+ASGI config for orderbot project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter # ts
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orderbot.settings")
+
+# application = get_asgi_application()
+django_asgi_app = get_asgi_application() # ts
+
+# ts
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+})
+
